c2ba4c61801b4449758fdd7af9966685f9b58da4
AadilKhanPathan <aadilkhanpathan937128@gmail.com>
2026-06-29 16:29:45 +0530

    Update RandomQuotes.jsx

--- src/Components.jsx/RandomQuotes.jsx
+++ src/Components.jsx/RandomQuotes.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import refreshIcon from "../assets/refresh-icon.png";
+import React, { useState, useEffect } from "react";
+import refreshIcon from "../assets/refresh.png";
 import twitter from "../assets/twitter.png";
 
 function RandomQuotes() {
+  const [isLoading, setisLoading] = useState(false);
   const [quote, setQuote] = useState({
-    text: "Do the best you can until you know better. Then when you know better, do better",
-    author: "maya angelou",
+    text: "",
+    author: "",
   });
 
   async function getRandomQuote() {
+    setisLoading(true)
     try {
       const response = await fetch("https://dummyjson.com/quotes/random");
       if (!response.ok) {
@@ -23,30 +24,30 @@ function RandomQuotes() {
       });
     } catch (error) {
       console.log(error);
+    } finally {
+      setisLoading(false)
     }
   }
   useEffect(() => {
     getRandomQuote();
   }, []);
 
-  function opentwitter(){
+  function openTwitter(){
     const tweetText = `${quote.text}  - ${quote.author}`;
     
     window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText )}`, 
     "tweet Window", 
     "width=600, height=600")
-
-    
   }
 
   return (
     <div className="container">
-      <div className="quote">{quote.text}</div>
+      <div className="quote">{isLoading ? "fetching wisdom" : quote.text}</div>
       <div className="bottom">
-        <div className="nameOfAuthor"> - {quote.author}</div>
+        <div className="nameOfAuthor"> {isLoading ? "..." : `- ${quote.author}`}</div>
         <div className="icons">
-          <img src={refreshIcon} onClick={getRandomQuote} alt="" />
-          <img src={twitter} onClick={opentwitter} alt="" />
+          <img src={refreshIcon} onClick={getRandomQuote} alt="Refresh quote" style={{ opacity: isLoading ? 0.5 : 1, cursor: isLoading ? 'default' : 'pointer' }} />
+          <img src={twitter} onClick={openTwitter} alt="Share on Twitter" />
         </div>
       </div>
     </div>
